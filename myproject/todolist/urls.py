7822a453ee04a9f730eb25706f4554b9ce288b19
jangwonjun <won2005won@naver.com>
2024-10-31 12:38:59 +0900

    [Refactor] urls.py 수정

--- myproject/todolist/urls.py
+++ myproject/todolist/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add/', views.add_view),
-    path('delete/', views.delete_view),
-    path('modify/', views.modify_view),
-    path('todo/', views.todo_view),
-    path('nottodo/', views.not_todo_view),
-    path('todoview/',views.show_view),
-    path('all_delete/',views.all_delete_view)
+    path('add', views.add_view),
+    path('delete', views.delete_view),
+    path('modify', views.modify_view),
+    path('todo', views.todo_view),
+    path('not-todo', views.not_todo_view),
+    path('todo-view',views.show_view),
+    path('all-delete',views.all_delete_view)
 ]  
